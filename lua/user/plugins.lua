0df078f24ee0d13450bc46d4f3fd078e31f94c67
Avinash Sharma V <avinashsharmav91@gmail.com>
2022-11-09 17:35:05 +0530

    adding treesitter text objects

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -98,6 +98,7 @@ return packer.startup(function(use)
 
   -- Treesitter
   use { "nvim-treesitter/nvim-treesitter"}
+  use { 'nvim-treesitter/nvim-treesitter-textobjects'}
 
   -- Git
   use { "lewis6991/gitsigns.nvim"}
