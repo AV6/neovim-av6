49a79760f078d2054dbc1a2f23998cdb3824febb
Avinash Sharma V <avinashsharmav91@gmail.com>
2022-12-09 15:29:43 +0530

    fixes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,18 +65,21 @@ return packer.startup(function(use)
   -- Colorschemes
   use {"morhetz/gruvbox"}
   use {'folke/tokyonight.nvim'}
+  use "EdenEast/nightfox.nvim"
+  use "sainnhe/gruvbox-material"
+  use 'NLKNguyen/papercolor-theme'
 
   -- cmp plugins
-  use { "hrsh7th/nvim-cmp"} 
-  use { "hrsh7th/cmp-buffer"} 
-  use { "hrsh7th/cmp-path"} 
-  use { "saadparwaiz1/cmp_luasnip"} 
+  use { "hrsh7th/nvim-cmp"}
+  use { "hrsh7th/cmp-buffer"}
+  use { "hrsh7th/cmp-path"}
+  use { "saadparwaiz1/cmp_luasnip"}
   use { "hrsh7th/cmp-nvim-lsp"}
   use { "hrsh7th/cmp-nvim-lua"}
 
   -- snippets
   use { "L3MON4D3/LuaSnip"} 
-  use { "rafamadriz/friendly-snippets"} 
+  use { "rafamadriz/friendly-snippets"}
   use {"mlaursen/vim-react-snippets"}
 
   -- syntax
@@ -84,8 +87,8 @@ return packer.startup(function(use)
 
   -- LSP
   use { "neovim/nvim-lspconfig"} 
-  use { "williamboman/nvim-lsp-installer"} 
-  use { "jose-elias-alvarez/null-ls.nvim"} 
+  use { "williamboman/nvim-lsp-installer"}
+  use { "jose-elias-alvarez/null-ls.nvim"}
   use { "RRethy/vim-illuminate"}
 
   -- Telescope
