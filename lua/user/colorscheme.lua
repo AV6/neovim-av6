7c8a639e5187ba319159a9786327dea2e1f419e4
Avinash Sharma V <avinashsharmav91@gmail.com>
2022-12-14 22:29:03 +0530

    quickfix ui issues in tile wm

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,6 @@
-local colorscheme = "terafox"
---[[ vim.opt.background = "light" ]]
+--[[ local colorscheme = "terafox" ]]
+local colorscheme = "gruvbox-material"
+vim.opt.background = "light"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
